7b99d59b1c1e8375ca25cc7a4fd60a7e310e85c3
NancyPatel11 <nancypatel5757@gmail.com>
2025-06-11 14:54:10 +0530

    added basic navbar template

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import bookicon from "../assets/icons/logo.svg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+
+export const NavBar = () => {
+    return (
+        <div className='flex ibm-plex-sans-400 text-white items-center justify-between'>
+            <div className='flex items-center gap-3'>
+                <img src={bookicon} alt="book icon" className="h-8" />
+                <h3 className="ibm-plex-sans-500 text-3xl">Bookademia</h3>
+            </div>
+            <div>
+                <ul className='flex gap-8 text-xl'>
+                    <li className='text-yellow'><a href="/home">Home</a></li>
+                    <li><a href="/search">Search</a></li>
+                    <li><a href="/profile">User</a></li>
+                    <li><a href=""><FontAwesomeIcon icon={faArrowRightFromBracket} className='text-red-500'/></a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
