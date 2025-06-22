7a030d86ebe160834b3ed4cdb89ad77164527f66
NancyPatel11 <nancypatel5757@gmail.com>
2025-06-22 15:58:57 +0530

    added receipt generation and downloading it as png

--- frontend/src/pages/admin/BorrowRequests.jsx
+++ frontend/src/pages/admin/BorrowRequests.jsx
@@ -8,9 +8,12 @@ import closeIcon from '../../assets/icons/admin/close.svg'
 import swapIcon from '../../assets/icons/admin/arrow-swap.png'
 import approveIcon from '../../assets/icons/admin/approve.png'
 import receiptIcon from '../../assets/icons/admin/receipt.svg'
+import receiptBg from '../../assets/images/receipt-bg.png'
+import bookicon from "../../assets/icons/logo.svg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
 import { toast } from 'sonner'
+import html2canvas from 'html2canvas-pro'
 
 const getInitials = (name) => {
     if (!name) return "";
@@ -41,7 +44,29 @@ export const BorrowRequests = () => {
     const [selectedRequest, setSelectedRequest] = useState(null);
     const [sortOldestFirst, setSortOldestFirst] = useState(true);
     const [showApprovalModal, setShowApprovalModal] = useState(false);
+    const [showReceiptModal, setShowReceiptModal] = useState(false);
+    const [selectedReceiptRequest, setSelectedReceiptRequest] = useState(null);
     const [loading, setLoading] = useState(true);
+
+
+    const downloadReceiptAsPNG = () => {
+        const receiptElement = document.getElementById("receipt");
+        if (!receiptElement) {
+            console.error("Receipt element not found");
+            return;
+        }
+
+        html2canvas(receiptElement, { scale: 2 })
+            .then((canvas) => {
+                const link = document.createElement("a");
+                link.download = "receipt.png";
+                link.href = canvas.toDataURL("image/png");
+                link.click();
+            })
+            .catch((error) => {
+                console.error("Error generating image:", error);
+            });
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -303,7 +328,19 @@ export const BorrowRequests = () => {
                                                 }) : request.status === "Pending" ? "Approval Pending" : "Not Returned Yet"}
                                             </td>
                                             <td className="text-center align-middle">
-                                                <img src={receiptIcon} alt="receipt" className="h-6 mx-auto" />
+                                                {request.status !== "Pending" &&
+                                                    <button
+                                                        onClick={() => {
+                                                            setSelectedReceiptRequest({ request, book, student });
+                                                            setShowReceiptModal(true);
+                                                        }}
+                                                        className="hover:opacity-80 hover:cursor-pointer text-admin-primary-blue flex gap-1 bg-admin-bg p-2 rounded-sm"
+                                                    >
+                                                        <img src={receiptIcon} alt="receipt" className="h-6 mx-auto" />
+                                                        <p>Generate</p>
+                                                    </button>
+                                                }
+
                                             </td>
                                         </tr>
                                     );
@@ -349,10 +386,132 @@ export const BorrowRequests = () => {
                             </div>
                         )}
 
+                        {showReceiptModal && selectedReceiptRequest && (
+                            <div className="fixed inset-0 bg-black/50 z-50 flex justify-center items-center">
+                                <div
+                                    className="relative bg-white w-[625px] p-8 rounded-lg shadow-lg"
+                                >
+                                    <Button
+                                        onClick={() => setShowReceiptModal(false)}
+                                        className="absolute top-1 right-0 bg-transparent hover:bg-gray-100 shadow-none hover:cursor-pointer"
+                                    >
+                                        <img src={closeIcon} alt="close" className="h-3 w-3" />
+                                    </Button>
+
+                                    <div
+                                        id="receipt"
+                                        className='bg-admin-receipt-bg p-5'
+                                    >
+                                        <div
+                                            className="h-full bg-center bg-no-repeat p-8 text-white"
+                                            style={{
+                                                backgroundImage: `url(${receiptBg})`,
+                                                backgroundSize: 'cover',
+                                                backgroundPosition: 'top',
+                                            }}
+                                        >
+                                            <div className='flex items-center gap-3'>
+                                                <img src={bookicon} alt="book icon" className="h-8" />
+                                                <h3 className="ibm-plex-sans-500 text-3xl">
+                                                    <a href="/home">Bookademia</a>
+                                                </h3>
+                                            </div>
+
+                                            <div>
+                                                <h1 className='text-2xl mt-6'>Borrow Receipt</h1>
+                                                <p className='text-lg ibm-plex-sans-300 mt-2'>
+                                                    Date Issued:
+                                                    <span className='ps-2 ibm-plex-sans-500 text-yellow'>
+                                                        {new Date().toLocaleDateString("en-GB")}
+                                                    </span>
+                                                </p>
+                                                <p className='text-lg ibm-plex-sans-300 mt-2'>
+                                                    Issued for:
+                                                    <span className='ps-2 ibm-plex-sans-500 text-yellow'>
+                                                        {selectedReceiptRequest.student.fullName} ({selectedReceiptRequest.student.email})
+                                                    </span>
+                                                </p>
+                                            </div>
+
+                                            <hr className='my-5' style={{ border: "none", height: "1px", backgroundColor: "#D6E0FF1A" }} />
+
+                                            <div className='text-gray-300 ibm-plex-sans-300'>
+                                                <h1 className='text-xl text-white mb-2 ibm-plex-sans-500'>Book Details</h1>
+                                                <div className="space-y-1 pl-2">
+                                                    <div className="flex items-start gap-2">
+                                                        <span className="text-white ">•</span>
+                                                        <span>Title: <span className="ibm-plex-sans-500 text-white">{selectedReceiptRequest.book.title}</span></span>
+                                                    </div>
+                                                    <div className="flex items-start gap-2">
+                                                        <span className="text-white">•</span>
+                                                        <span>Author: <span className="ibm-plex-sans-500 text-white">{selectedReceiptRequest.book.author}</span></span>
+                                                    </div>
+                                                    <div className="flex items-start gap-2">
+                                                        <span className="text-white">•</span>
+                                                        <span>Genre: <span className="ibm-plex-sans-500 text-white">{selectedReceiptRequest.book.genre}</span></span>
+                                                    </div>
+                                                    <div className="flex items-start gap-2">
+                                                        <span className="text-white">•</span>
+                                                        <span>Borrowed On: <span className="ibm-plex-sans-500 text-white"> {new Date(selectedReceiptRequest.request.issueDate).toLocaleDateString()}</span></span>
+                                                    </div>
+                                                    <div className="flex items-start gap-2">
+                                                        <span className="text-white">•</span>
+                                                        <span>Due Date: <span className="ibm-plex-sans-500 text-white"> {new Date(selectedReceiptRequest.request.dueDate).toLocaleDateString()}</span></span>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+
+                                            <hr className='my-5' style={{ border: "none", height: "1px", backgroundColor: "#D6E0FF1A" }} />
+
+                                            <div className='text-gray-300 ibm-plex-sans-300'>
+                                                <h1 className='text-xl text-white mb-2 ibm-plex-sans-500'>Terms</h1>
+                                                <div className="space-y-1 pl-2">
+                                                    <div className="flex items-start gap-2">
+                                                        <span className="text-white">•</span>
+                                                        Ensure the book is returned by the due date.
+                                                    </div>
+                                                    <div className="flex items-start gap-2">
+                                                        <span className="text-white">•</span>
+                                                        Late returns may incur fines.
+                                                    </div>
+                                                    <div className="flex items-start gap-2">
+                                                        <span className="text-white">•</span>
+                                                        Lost or damaged books may incur replacement costs.
+                                                    </div>
+                                                    <div className="flex items-start gap-2">
+                                                        <span className="text-white">•</span>
+                                                        Contact us for any issues or concerns.
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <hr className='my-5' style={{ border: "none", height: "1px", backgroundColor: "#D6E0FF1A" }} />
+
+                                            <div className='text-gray-300 ibm-plex-sans-300'>
+                                                <p>Thank you for using <span className='ibm-plex-sans-500 text-white'>Bookademia!</span></p>
+                                                <p>Website: <span className='ibm-plex-sans-500 text-white'>bookademia.example.com</span></p>
+                                                <p>Email: <span className='ibm-plex-sans-500 text-white'>nancypatel5757@gmail.com</span></p>
+                                            </div>
+
+
+                                        </div>
+
+                                    </div>
+
+                                    <Button
+                                        onClick={downloadReceiptAsPNG}
+                                        className="mt-6 w-full bg-admin-primary-blue text-white hover:bg-admin-tertiary-blue hover:cursor-pointer"
+                                    >
+                                        Download Receipt
+                                    </Button>
+                                </div>
+                            </div>
+                        )}
 
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
