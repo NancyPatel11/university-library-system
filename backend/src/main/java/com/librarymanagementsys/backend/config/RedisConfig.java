0ccb14e2e2a1b99740bb288a80721318a4276967
NancyPatel11 <nancypatel5757@gmail.com>
2025-07-03 16:37:27 +0530

    Fix: added creation of redisconnectionfactory based on redis url

--- backend/src/main/java/com/librarymanagementsys/backend/config/RedisConfig.java
+++ backend/src/main/java/com/librarymanagementsys/backend/config/RedisConfig.java
@@ -1,12 +1,24 @@
 package com.librarymanagementsys.backend.config;
 
+import io.lettuce.core.RedisURI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.url}")
+    private String redisUrl;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisURI uri = RedisURI.create(redisUrl);
+        return new LettuceConnectionFactory(uri.getHost(), uri.getPort());
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
